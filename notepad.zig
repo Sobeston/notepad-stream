3612f16c6fe602c4b2e8fa63ea73d67ef1fd2c56
Sebastian <15335529+Sobeston@users.noreply.github.com>
2020-05-21 00:01:57 +0100

    refactor

--- notepad.zig
+++ notepad.zig
@@ -5,31 +5,25 @@ extern "User32" fn FindWindowA(lpClassName: ?[*:0]const u8, lpWindowName: ?[*:0]
 extern "User32" fn FindWindowExA(hWndParent: ?win.HANDLE, hWndChildAfter: ?win.HANDLE, lpszClass: ?[*:0]const u8, lpszWindow: ?[*:0]const u8) callconv(.Stdcall) ?*c_void;
 extern "User32" fn SendMessageA(hWnd: ?win.HANDLE, msg: win.UINT, wParam: win.WPARAM, lParam: win.LPARAM) callconv(.Stdcall) win.LRESULT;
 
-pub const NotepadOutStream = struct {
-    pub const errors = error{ NotepadNotFound, EditPanelNotFound };
-    pub const OutStream = std.io.OutStream(*@This(), errors, write);
+pub const errors = error{ NotepadNotFound, EditPanelNotFound };
+pub const OutStream = std.io.OutStream(void, errors, write);
 
-    pub fn init() @This() {
-        return .{};
-    }
+pub fn write(self: void, bytes: []const u8) errors!usize {
+    const EM_REPLACESEL = 0xC2;
+    _ = SendMessageA(
+        FindWindowExA(
+            FindWindowA(null, "Untitled - Notepad") orelse return error.NotepadNotFound,
+            null,
+            "EDIT",
+            null,
+        ) orelse return error.EditPanelNotFound,
+        EM_REPLACESEL,
+        1,
+        @intToPtr(win.LPARAM, @ptrToInt(bytes.ptr)),
+    );
+    return bytes.len;
+}
 
-    pub fn outStream(self: *@This()) OutStream {
-        return .{ .context = self };
-    }
-
-    pub fn write(self: *@This(), bytes: []const u8) errors!usize {
-        const EM_REPLACESEL = 0xC2;
-        _ = SendMessageA(
-            FindWindowExA(
-                FindWindowA(null, "Untitled - Notepad") orelse return error.NotepadNotFound,
-                null,
-                "EDIT",
-                null,
-            ) orelse return error.EditPanelNotFound,
-            EM_REPLACESEL,
-            1,
-            @intToPtr(win.LPARAM, @ptrToInt(bytes.ptr)),
-        );
-        return bytes.len;
-    }
-};
+pub fn outStream() OutStream {
+    return .{ .context = void{} };
+}
