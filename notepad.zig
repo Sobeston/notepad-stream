0cfc0b47e0c739298f2492165b38339ee0f2321d
Sebastian <15335529+Sobeston@users.noreply.github.com>
2020-05-21 00:52:42 +0100

    fix termination issue / UB

--- notepad.zig
+++ notepad.zig
@@ -3,12 +3,22 @@ const win = std.os.windows;
 
 extern "User32" fn FindWindowA(lpClassName: ?[*:0]const u8, lpWindowName: ?[*:0]const u8) callconv(.Stdcall) ?*c_void;
 extern "User32" fn FindWindowExA(hWndParent: ?win.HANDLE, hWndChildAfter: ?win.HANDLE, lpszClass: ?[*:0]const u8, lpszWindow: ?[*:0]const u8) callconv(.Stdcall) ?*c_void;
-extern "User32" fn SendMessageA(hWnd: ?win.HANDLE, msg: win.UINT, wParam: win.WPARAM, lParam: win.LPARAM) callconv(.Stdcall) win.LRESULT;
+extern "User32" fn SendMessageA(hWnd: ?win.HANDLE, msg: win.UINT, wParam: win.WPARAM, lParam: ?[*:0]const u8) callconv(.Stdcall) win.LRESULT;
 
-pub const errors = error{ NotepadNotFound, EditPanelNotFound };
-pub const OutStream = std.io.OutStream(void, errors, write);
+pub const errors = @typeInfo(@typeInfo(@TypeOf(write)).Fn.return_type.?).ErrorUnion.error_set;
+pub const OutStream = std.io.OutStream(@This(), errors, write);
 
-pub fn write(self: void, bytes: []const u8) errors!usize {
+mem: *std.mem.Allocator,
+
+pub fn init(allocator: *std.mem.Allocator) @This() {
+    return .{ .mem = allocator };
+}
+
+pub fn write(self: @This(), bytes: []const u8) !usize {
+    const buf = try self.mem.allocSentinel(u8, bytes.len, 0);
+    std.mem.copy(u8, buf, bytes);
+    defer self.mem.free(buf);
+
     const EM_REPLACESEL = 0xC2;
     _ = SendMessageA(
         FindWindowExA(
@@ -19,11 +29,11 @@ pub fn write(self: void, bytes: []const u8) errors!usize {
         ) orelse return error.EditPanelNotFound,
         EM_REPLACESEL,
         1,
-        @intToPtr(win.LPARAM, @ptrToInt(bytes.ptr)),
+        buf.ptr,
     );
     return bytes.len;
 }
 
-pub fn outStream() OutStream {
-    return .{ .context = void{} };
+pub fn outStream(self: @This()) OutStream {
+    return .{ .context = self };
 }
